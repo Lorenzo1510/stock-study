dfdda046cfd008d839527ecf7d43e85a39679671
Lorenzo Catanzaro <lorenzo.c15@hotmail.it>
2024-12-14 11:03:46 +0100

    tolta classe FeedBack e aggiunto import

--- src/model/lstm_pred.py
+++ src/model/lstm_pred.py
@@ -10,6 +10,7 @@ from tensorflow.keras.models import Sequential # type: ignore
 from tensorflow.keras.layers import LSTM, Dense # type: ignore
 
 from src.model.modelABC import ModelABC
+from src.model.feed_back import FeedBack
 
 
 class TimeSeriesPredictor(ModelABC):
@@ -165,33 +166,3 @@ class TimeSeriesPredictor(ModelABC):
         y_test_denorm = self.scaler.inverse_transform(self.y_test.reshape(-1, 1)) 
         mae = mean_absolute_error(y_test_denorm[-self.out_steps:], predictions) 
         return mae
-    
-
-class FeedBack(tf.keras.Model):
-    def __init__(self, units: int, out_steps: int, num_features: int):
-        super().__init__()
-        self.out_steps = out_steps
-        self.units = units
-        self.lstm_cell = tf.keras.layers.LSTMCell(units)
-        self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True)
-        self.dense = tf.keras.layers.Dense(num_features)
-
-    def warmup(self, inputs: tf.Tensor) -> Tuple[tf.Tensor, list]:
-        x, *state = self.lstm_rnn(inputs)
-        prediction = self.dense(x)
-        return prediction, state
-
-    def call(self, inputs: tf.Tensor, training=None) -> tf.Tensor:
-        predictions = []
-        prediction, state = self.warmup(inputs)
-        predictions.append(prediction)
-        
-        for _ in range(1, self.out_steps):
-            x = prediction
-            x, state = self.lstm_cell(x, states=state, training=training)
-            prediction = self.dense(x)
-            predictions.append(prediction)
-        
-        predictions = tf.stack(predictions)
-        predictions = tf.transpose(predictions, [1, 0, 2])
-        return predictions
